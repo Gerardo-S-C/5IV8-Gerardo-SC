c1623d28b444df0d45e8c7e6866768baa7d7c8c9
GerardoS-C <gsandovalc1900@alumno.ipn.com>
2021-10-07 21:12:49 -0500

    Cifrados DES

---
+++ DES_tarea/src/des_tarea/DES_tarea.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package des_tarea;
+
+/**
+ *
+ * @author illum
+ */
+public class DES_tarea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cifrado cif=new Cifrado();
+        cif.setVisible(true);
+    }
+    
+}
